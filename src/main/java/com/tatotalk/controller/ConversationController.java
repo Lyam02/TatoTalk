c5584d8838b7df89c6accbf68935663d295407bd
Lyam02 <sauvage.lyam@outlook.fr>
2025-11-13 08:53:01 +0100

    first message

--- src/main/java/com/tatotalk/controller/ConversationController.java
+++ src/main/java/com/tatotalk/controller/ConversationController.java
@@ -1,5 +1,8 @@
 package com.tatotalk.controller;
 
+import com.tatotalk.model.Messages;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -7,6 +10,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
 
 @WebServlet("/conv")
 public class ConversationController extends HttpServlet {
@@ -18,4 +22,24 @@ public class ConversationController extends HttpServlet {
                 .forward(request, response);
     }
 
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+        EntityManagerFactory emf = (EntityManagerFactory) getServletContext().getAttribute("emf");
+        EntityManager em = emf.createEntityManager();
+
+        String messageText = request.getParameter("message");
+
+        Messages message = new Messages();
+        message.setMessage_content(messageText);
+        message.setCreated_at(LocalDateTime.now());
+
+        em.getTransaction().begin();
+
+        em.persist(message);
+
+        em.getTransaction().commit();
+
+        response.sendRedirect("index.jsp");
+    }
 }
